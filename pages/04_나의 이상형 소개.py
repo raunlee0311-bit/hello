7bb0d392957ee35a9fa6d2c8a4bda76723ad04ca
raunlee0311-bit <raunlee0311@gmail.com>
2026-07-11 11:23:53 +0900

    Create 04_나의 이상형 소개.py

---
+++ pages/04_나의 이상형 소개.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.set_page_config(page_title="이상형 소개")
+
+st.title("❤️ 나의 이상형")
+
+st.write("""
+제가 중요하게 생각하는 이상형은 외적인 모습보다
+성격과 가치관입니다.
+""")
+
+st.markdown("""
+✔ 배려심이 있는 사람
+
+✔ 대화를 잘하는 사람
+
+✔ 긍정적인 사람
+
+✔ 서로를 존중하는 사람
+
+✔ 웃는 모습이 예쁜 사람
+""")
