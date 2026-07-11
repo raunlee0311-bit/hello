deee5775921ee96e0196db7af6c7a0761dcdeb19
raunlee0311-bit <raunlee0311@gmail.com>
2026-07-11 11:23:23 +0900

    Create 03_취미소개.py

---
+++ pages/03_취미소개.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(page_title="취미 소개")
+
+st.title("🎮 나의 취미")
+
+st.write("""
+제가 가장 좋아하는 취미는 다음과 같습니다.
+""")
+
+st.markdown("""
+- 🎮 게임하기
+- 🎵 음악 듣기
+- 📚 독서
+- 💻 프로그래밍
+- 🚶 산책
+""")
+
+st.info("취미를 통해 스트레스를 해소하고 새로운 아이디어를 얻습니다.")
